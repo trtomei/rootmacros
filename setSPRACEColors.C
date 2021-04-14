8279f03f7e63bdbee36ec4321c18fec5e9e9bfc6
Thiago Tomei Fernandez <thiago.tomei@cern.ch>
2021-04-14 11:01:49 -0300

    setSPRACEColors.C and .gitignore

---
+++ setSPRACEColors.C
@@ -0,0 +1,27 @@
+{
+   enum SPcolor {
+      SPred,
+      SPdred,
+      SPgreen,
+      SPdgreen,
+      SPblue,
+      SPdblue,
+      SPyellow,
+      SPdyellow,
+      SPbeige,
+      SPdbeige,
+      SPbrown,
+      SPdbrown,
+      SPgrey,
+      SPdgrey,
+      SPpurple,
+   };
+
+   int SPcolors[15] = {
+      TColor::GetColor(177, 4, 14),    TColor::GetColor(140, 21, 21),  TColor::GetColor(0, 155, 118),
+      TColor::GetColor(23, 94, 84),    TColor::GetColor(0, 152, 219),  TColor::GetColor(0, 124, 146),
+      TColor::GetColor(234, 171, 0),   TColor::GetColor(178, 111, 22), TColor::GetColor(210, 194, 149),
+      TColor::GetColor(179, 153, 93),  TColor::GetColor(141, 60, 30),  TColor::GetColor(94, 48, 50),
+      TColor::GetColor(182, 177, 169), TColor::GetColor(77, 79, 83),   TColor::GetColor(83, 40, 79),
+   };
+}
